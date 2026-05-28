2481c53f725cfb96ab135760a79a30172ea0ba7e
SebasGL <sebasgonzalez014@gmail.com>
2026-05-28 00:43:05 -0500

    aGREGAR ASYNC EN NOTIFICATIONS APLICATION

--- sportshop-notifications/src/main/java/com/sportshop/notifications/NotificationsApplication.java
+++ sportshop-notifications/src/main/java/com/sportshop/notifications/NotificationsApplication.java
@@ -2,8 +2,10 @@ package com.sportshop.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NotificationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationsApplication.class, args);
